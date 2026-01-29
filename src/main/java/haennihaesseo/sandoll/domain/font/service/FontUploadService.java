e5989e652fbb52a31d644ed95ec964ce4d92f2e4
oosedus <seayeon0729@gmail.com>
2026-01-29 13:30:36 +0900

    feat : S3에 폰트 올리는 백엔드용 API 생성, font type nullable로 우선 설정 - 추후 수정 예정

---
+++ src/main/java/haennihaesseo/sandoll/domain/font/service/FontUploadService.java
@@ -0,0 +1,44 @@
+package haennihaesseo.sandoll.domain.font.service;
+
+
+import haennihaesseo.sandoll.domain.font.entity.Font;
+import haennihaesseo.sandoll.domain.font.repository.FontRepository;
+import haennihaesseo.sandoll.global.infra.AwsS3Client;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FontUploadService {
+
+  private final FontRepository fontRepository;
+  private final AwsS3Client s3Client;
+
+  /**
+   * 폰트 파일 업로드 및 DB 저장
+   * @param fonts
+   * @param fontNames
+   * @return 업로드된 폰트 개수
+   */
+  public int uploadFonts(List<MultipartFile> fonts, List<String> fontNames, String directory) {
+    int uploadCount = 0;
+    for (MultipartFile font : fonts) {
+      String fileUrl = s3Client.uploadFile(directory, font);
+
+      // 폰트 저장 -> 키워드 어떻게 할지 고민
+      fontRepository.save(Font.builder()
+          .fontUrl(fileUrl)
+          .name(fontNames.get(uploadCount))
+          .price(0)
+          .build());
+
+      uploadCount++;
+    }
+    return uploadCount;
+  }
+
+}
