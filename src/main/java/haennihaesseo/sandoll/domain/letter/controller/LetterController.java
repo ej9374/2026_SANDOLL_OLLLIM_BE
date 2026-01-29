3f3a21ea853416e0050b2cbd8eed0a633c4970f7
oosedus <seayeon0729@gmail.com>
2026-01-29 17:04:10 +0900

    refactor: LetterKey 삭제, LetterId 헤더 조회로 변경

--- src/main/java/haennihaesseo/sandoll/domain/letter/controller/LetterController.java
+++ src/main/java/haennihaesseo/sandoll/domain/letter/controller/LetterController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -44,11 +43,10 @@ public class LetterController {
   )
   @PatchMapping("/{letterId}")
   public ResponseEntity<ApiResponse<Void>> inputLetterInfo(
-      @RequestHeader("letterKey") String letterKey,
-      @PathVariable String letterId,
+      @RequestHeader("letterId") String letterId,
       @RequestBody @Valid LetterInfoRequest request
   ) {
-    letterService.inputLetterInfo(letterId, letterKey, request);
+    letterService.inputLetterInfo(letterId, request);
 
     return ApiResponse.success(LetterSuccessStatus.SUCCESS_302);
   }
