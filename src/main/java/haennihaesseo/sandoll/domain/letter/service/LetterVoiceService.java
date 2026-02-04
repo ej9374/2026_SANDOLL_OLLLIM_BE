8dd43ac2d42045f731910ca1efa026ebcac519f1
oosedus <seayeon0729@gmail.com>
2026-02-04 17:19:55 +0900

    refactor: 음성 분석 중복 요청 방지 및 Redis 락 추가

--- src/main/java/haennihaesseo/sandoll/domain/letter/service/LetterVoiceService.java
+++ src/main/java/haennihaesseo/sandoll/domain/letter/service/LetterVoiceService.java
@@ -8,6 +8,7 @@ import haennihaesseo.sandoll.domain.letter.converter.LetterConverter;
 import haennihaesseo.sandoll.domain.letter.dto.response.VoiceAnalysisResponse;
 import haennihaesseo.sandoll.domain.letter.exception.LetterException;
 import haennihaesseo.sandoll.domain.letter.status.LetterErrorStatus;
+import haennihaesseo.sandoll.global.infra.RedisClient;
 import haennihaesseo.sandoll.global.infra.python.PythonAnalysisClient;
 import haennihaesseo.sandoll.global.infra.python.dto.PythonVoiceAnalysisRequest;
 import haennihaesseo.sandoll.global.infra.python.dto.PythonVoiceAnalysisResponse;
@@ -25,40 +26,56 @@ public class LetterVoiceService {
   private final PythonAnalysisClient pythonAnalysisClient;
   private final FontRepository fontRepository;
   private final LetterConverter letterConverter;
+  private final RedisClient redisClient;
 
   private final int RECOMMEND_FONT_COUNT = 3;
+  private final long TIMEOUT_MS = 180000; // 3분
 
   public VoiceAnalysisResponse analyzeVoice(String letterId) {
-    //TODO: 이미 분석하고 있거나 분석된 경우 재분석 막기
-
     // Redis에서 CachedLetter 조회
     CachedLetter cachedLetter = cachedLetterRepository.findById(letterId)
         .orElseThrow(() -> new LetterException(LetterErrorStatus.LETTER_NOT_FOUND));
 
-    // 파이썬 서버에 분석 요청
-    PythonVoiceAnalysisRequest request = letterConverter.toAnalysisRequest(cachedLetter);
-    PythonVoiceAnalysisResponse pythonResponse = pythonAnalysisClient.requestVoiceAnalysis(request);
-
-    log.info("[분석 완료] letterId={}, 추천 폰트: {}", letterId, pythonResponse.getRecommendedFonts());
-
-    // 추천 폰트 정보 이름으로 매칭
-    List<Font> recommendedFonts = fontRepository.findByNameIn(pythonResponse.getRecommendedFonts());
-
-    // 분석된 폰트 결과 저장
-    cachedLetter.setVoiceFonts(recommendedFonts, pythonResponse.getVoiceKeywords());
-
-    // 앞에 3개 폰트만 추천으로 설정
-    if (recommendedFonts.size() > RECOMMEND_FONT_COUNT) {
-      recommendedFonts = recommendedFonts.subList(0, RECOMMEND_FONT_COUNT);
+    //이미 분석 완료
+    if (cachedLetter.getVoiceFontIds() != null && !cachedLetter.getVoiceFontIds().isEmpty()) {
+      throw new LetterException(LetterErrorStatus.ALREADY_ANALYZED);
     }
 
-    // 현재 추천된 폰트 정보 캐쉬에 저장
-    cachedLetter.setCurrentRecommendFontIds(recommendedFonts.stream().map(Font::getFontId).toList());
-    // shown 폰트 업데이트
-    cachedLetter.setShownVoiceFontIds(recommendedFonts.stream().map(Font::getFontId).toList());
+    // 락 획득
+    boolean acquired = redisClient.acquireLock("VOICE_ANALYSIS_LOCK:" + letterId, "ANALYZING", TIMEOUT_MS);
+    if(!acquired) {
+      throw new LetterException(LetterErrorStatus.ALREADY_ANALYZED);
+    }
 
-    cachedLetterRepository.save(cachedLetter);
+    try {
+      // 파이썬 서버에 분석 요청
+      PythonVoiceAnalysisRequest request = letterConverter.toAnalysisRequest(cachedLetter);
+      PythonVoiceAnalysisResponse pythonResponse = pythonAnalysisClient.requestVoiceAnalysis(request);
 
-    return letterConverter.toVoiceAnalysisResponse(pythonResponse.getAnalysisResult(), recommendedFonts);
+      log.info("[분석 완료] letterId={}, 추천 폰트: {}", letterId, pythonResponse.getRecommendedFonts());
+
+      // 추천 폰트 정보 이름으로 매칭
+      List<Font> recommendedFonts = fontRepository.findByNameIn(pythonResponse.getRecommendedFonts());
+
+      // 분석된 폰트 결과 저장
+      cachedLetter.setVoiceFonts(recommendedFonts, pythonResponse.getVoiceKeywords());
+
+      // 앞에 3개 폰트만 추천으로 설정
+      if (recommendedFonts.size() > RECOMMEND_FONT_COUNT) {
+        recommendedFonts = recommendedFonts.subList(0, RECOMMEND_FONT_COUNT);
+      }
+
+      // 현재 추천된 폰트 정보 캐쉬에 저장
+      cachedLetter.setCurrentRecommendFontIds(recommendedFonts.stream().map(Font::getFontId).toList());
+      // shown 폰트 업데이트
+      cachedLetter.setShownVoiceFontIds(recommendedFonts.stream().map(Font::getFontId).toList());
+
+      cachedLetterRepository.save(cachedLetter);
+
+      return letterConverter.toVoiceAnalysisResponse(pythonResponse.getAnalysisResult(), recommendedFonts);
+    } finally {
+      // 락 해제
+      redisClient.releaseLock("VOICE_ANALYSIS_LOCK:" + letterId);
+    }
   }
 }
